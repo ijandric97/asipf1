a16401d38bf21d11c456f37ae68f29d7e5457e70
Ivan Jandrić <ijandric97@gmail.com>
2023-02-28 10:58:41 +0100

    Gap DNF Results analysis and correlation

--- asipf1/core/constants.py
+++ asipf1/core/constants.py
@@ -6,6 +6,7 @@ DATA_FOLDER = "data"
 PERCENTAGES_CSV = DATA_FOLDER + "./percentages.csv"
 GAPS_CSV = DATA_FOLDER + "./gaps.csv"
 PITSTOPS_CSV = DATA_FOLDER + "./pitstops.csv"
+RESULTS_CSV = DATA_FOLDER + "./results.csv"
 
 IMAGES_FOLDER = "images"
 IMAGES_DNFS_FOLDER = IMAGES_FOLDER + "/gap_dnfs"
@@ -14,6 +15,8 @@ IMAGES_PITSTOPS_FOLDER = IMAGES_FOLDER + "/optimal_pitstop"
 IMAGES_DPI = 100
 IMAGES_DNFS_SIZE = (16, 10)
 IMAGES_PITSTOPS_SIZE = (16, 20)
+
+BAR_WIDTH = 0.35
 
 # We are looking for the following statueses:
 # 3 - Accident -> Neš se desilo i DNF je ishod, znači najčešće izletio u zid (Senna 94)
@@ -28,8 +31,17 @@ IMAGES_PITSTOPS_SIZE = (16, 20)
 #                           continue running for additional lap(s) until the car gave up
 # 138 - Debris -> Only 1 such case, which is Russell 2020 when he got hit by a loose
 #                 wheel from another car
+#
+# 'Good' finished statuses for the 1996 onwards are:
+# 1 - Finished
+# 11 - +1 Lap
+# 12 - +2 Laps
+# ...and so on till
+# 18 - +8 Laps
 STATUS_ACCIDENTS = [3, 20, 104]
 STATUS_COLLISIONS = [4, 130, 138]
+STATUS_FINISHED = [1, 11, 12, 13, 14, 15, 16, 17, 18]
+POSITION_DNF = ["R", "D", "E", "W", "F", "N"]
 
 
 class PlotData(TypedDict):
--- asipf1/gap_dnf.py
+++ asipf1/gap_dnf.py
@@ -1,23 +1,30 @@
 import os
 import textwrap
+import warnings
 
 import ergast
 import numpy as np
 import pandas as pd
 from core.constants import (
+    BAR_WIDTH,
     GAPS_CSV,
     IMAGES_DNFS_FOLDER,
     IMAGES_DNFS_SIZE,
     IMAGES_DPI,
     PERCENTAGES_CSV,
+    POSITION_DNF,
+    RESULTS_CSV,
     STATUS_ACCIDENTS,
     STATUS_COLLISIONS,
+    STATUS_FINISHED,
 )
 from matplotlib import pyplot as plt
 
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
-def generate_dataset() -> pd.DataFrame:
-    results = pd.DataFrame([])
+
+def generate_dataset() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    result_gaps = []
     percentages = pd.DataFrame([])
     gaps = pd.DataFrame([])
 
@@ -32,12 +39,12 @@ def generate_dataset() -> pd.DataFrame:
         race_count = len(ergast.race_schedule(year=year).index)
 
         for race in range(1, race_count + 1):  # Rounds are 1-index based
-            race_percentages, race_gaps = get_race_dnfs(year, race)
+            race_percentages, race_gaps, result_gap = get_race_dnfs(year, race)
 
             percentages = pd.concat([percentages, race_percentages])
             gaps = pd.concat([gaps, race_gaps])
-
-            # results = pd.concat([results, get_dnfs_data(year, race)])
+            if result_gap > 0:  # -1 means we terminated the iteration
+                result_gaps.append(result_gap)
 
     # Convert to int64 and summ everything without duplicates
     percentages.sort_values(by=["percentage"], inplace=True)
@@ -62,21 +69,21 @@ def generate_dataset() -> pd.DataFrame:
     gaps.reset_index(inplace=True, drop=True)
     gaps.to_csv(GAPS_CSV, index=False)
 
-    # FIXME
-    # # Concat messed up index, so reset it
-    # results.reset_index(inplace=True, drop=True)
-    # results.to_csv(DNFS_CSV, index=False)
+    result_gaps.sort()
+    results = pd.DataFrame(result_gaps, columns=["gap"])
+    results.to_csv(RESULTS_CSV, index=False)
 
-    return percentages, gaps
+    return percentages, gaps, results
 
 
-def get_race_dnfs(year: int, race: int) -> tuple[pd.DataFrame, pd.DataFrame]:
+def get_race_dnfs(year: int, race: int) -> tuple[pd.DataFrame, pd.DataFrame, int]:
     lap_times = ergast.lap_times(year, race)
     if lap_times.empty:
         print(f"SKIPPING ({year}:{race}) -> No lap_time data")
         return (
             pd.DataFrame([]),
             pd.DataFrame([]),
+            -1,
         )  # Skip because no lap time data is available
     lap_count = lap_times["lap"].max()
 
@@ -98,6 +105,10 @@ def get_race_dnfs(year: int, race: int) -> tuple[pd.DataFrame, pd.DataFrame]:
         race_results.query(f"statusId in {STATUS_COLLISIONS}").reset_index(drop=True)
     )["laps"].to_list()
 
+    finished_drivers = race_results.query(
+        f"positionText not in {POSITION_DNF} and statusId in {STATUS_FINISHED}"
+    )["driverId"].to_list()
+
     race_percentages = pd.DataFrame([])
     race_gaps = pd.DataFrame([])
 
@@ -110,7 +121,18 @@ def get_race_dnfs(year: int, race: int) -> tuple[pd.DataFrame, pd.DataFrame]:
         race_percentages = pd.concat([race_percentages, percentage])
         race_gaps = pd.concat([race_gaps, gap])
 
-    return race_percentages, race_gaps
+    # Get median gap to the leader at the end of the race :)
+    grouped = lap_times.query(f"driverId in {finished_drivers}").groupby(["driverId"])
+    results = pd.DataFrame([])
+    for _, group in grouped:
+        results = pd.concat([results, group[group["lap"] == group["lap"].max()]])
+
+    result_gap = int(
+        results["total_millis"].median()  # We are using MEDIAN
+        - results["total_millis"].min()
+    )
+
+    return race_percentages, race_gaps, result_gap
 
 
 def get_lap_dnfs(
@@ -191,12 +213,7 @@ def _analyze_percentages(df: pd.DataFrame) -> None:
     plt.xlabel("Percentage of the race completed")
     plt.ylabel("DNFS")
     plt.text(
-        0.5,
-        0.99,
-        text,
-        ha="center",
-        va="top",
-        transform=ax.transAxes,
+        0.5, 0.99, text, ha="center", va="top", transform=ax.transAxes, fontsize=12
     )
     print(text)
     plt.ylim([0, 35])
@@ -206,28 +223,137 @@ def _analyze_percentages(df: pd.DataFrame) -> None:
     )
 
 
-def _analyze_gaps(df: pd.DataFrame) -> None:
+def _analyze_gaps(gaps: pd.DataFrame, results: pd.DataFrame) -> None:
     """Analyzes correlation between DNF Accidents/Collisions and completion percentage.
 
     Args:
         df (pd.DataFrame): DataFrame containing accidents, collisions and median gaps.
     """
+    ta = gaps["total_accidents"].max()
+    tc = gaps["total_collisions"].max()
 
     # Split into bins of equal time gaps and calculate sum of accidents and collisions
     # in them. We will draw that in bar plot.
-    df["bins"] = pd.qcut(df["gap"], q=10)
-    grouped = df.groupby(["bins"])
+    gaps["bins"], bins = pd.qcut(gaps["gap"], q=10, retbins=True)
     res = pd.DataFrame()
+    grouped = gaps.groupby(["bins"])
     for name, group in grouped:
-        bin_label = pd.to_datetime(name.right.astype(np.int64), unit="ms").strftime(
-            "%M:%S:%f"
-        )[:-3]
-
         item = pd.DataFrame(
             [
                 {
                     "bin": name,
-                    "binLabel": bin_label,
+                    "binLabel": pd.to_datetime(
+                        name.right.astype(np.int64), unit="ms"
+                    ).strftime("%M:%S:%f")[:-3],
+                    "accidents": group["accidents"].sum(),
+                    "collisions": group["collisions"].sum(),
+                }
+            ]
+        )
+        res = pd.concat([res, item])
+    gaps = res
+
+    results["bins"] = pd.cut(results["gap"], bins=bins)
+    res = pd.DataFrame()
+    grouped = results.groupby(["bins"])
+    for name, group in grouped:
+        item = pd.DataFrame(
+            [
+                {
+                    "bin": name,
+                    "binLabel": pd.to_datetime(
+                        name.right.astype(np.int64), unit="ms"
+                    ).strftime("%M:%S:%f")[:-3],
+                    "finishes": len(group),
+                }
+            ]
+        )
+        res = pd.concat([res, item])
+
+    gaps = (
+        gaps.merge(res, on=["binLabel"], how="left")
+        .drop(columns=["bin_y"])
+        .rename(columns={"bin_x": "bin"})
+    )
+    del res
+
+    corr_fa = round(gaps["finishes"].corr(gaps["accidents"]) * 100, 2)
+    corr_fc = round(gaps["finishes"].corr(gaps["collisions"]) * 100, 2)
+    corr_ac = round(gaps["accidents"].corr(gaps["collisions"]) * 100, 2)
+    text = textwrap.dedent(
+        f"""
+        {ta} Accidents and {tc} Collisions.
+        Bins determined by accidents and collisions count.
+        Correlation: (FA: {corr_fa}%, FC: {corr_fc}%, AC: {corr_ac}%).
+        """
+    )
+
+    x = np.arange(len(gaps["binLabel"]))
+    _, ax = plt.subplots(figsize=IMAGES_DNFS_SIZE, dpi=IMAGES_DPI)
+    ax.bar(
+        x - BAR_WIDTH / 2, gaps["accidents"], BAR_WIDTH, label="Accidents", color="g"
+    )
+    ax.bar(x, gaps["finishes"], BAR_WIDTH, label="Finishes", color="b")
+    ax.bar(
+        x + BAR_WIDTH / 2, gaps["collisions"], BAR_WIDTH, label="Collisions", color="r"
+    )
+    ax.set_xticks(x + BAR_WIDTH * 1.5)
+    ax.set_xticklabels(gaps["binLabel"])
+    ax.legend()
+    plt.title("Accidents and Collisions over median gap to leader")
+    plt.xlabel("Median gap to leader (upper/right interval limit)")
+    t = plt.text(
+        0.5,
+        0.99,
+        text,
+        ha="center",
+        va="top",
+        transform=ax.transAxes,
+        fontsize=12,
+    )
+    t.set_bbox(dict(facecolor="white", alpha=0.5))
+    print(text)
+    plt.savefig(
+        f"{IMAGES_DNFS_FOLDER}/gaps.png",
+        dpi=IMAGES_DPI,
+    )
+
+
+def _analyze_results(results: pd.DataFrame, gaps: pd.DataFrame) -> None:
+    ta = gaps["total_accidents"].max()
+    tc = gaps["total_collisions"].max()
+
+    # Split into bins of equal time gaps and calculate sum of accidents and collisions
+    # in them. We will draw that in bar plot.
+    results["bins"], bins = pd.qcut(results["gap"], q=10, retbins=True)
+    res = pd.DataFrame()
+    grouped = results.groupby(["bins"])
+    for name, group in grouped:
+        item = pd.DataFrame(
+            [
+                {
+                    "bin": name,
+                    "binLabel": pd.to_datetime(
+                        name.right.astype(np.int64), unit="ms"
+                    ).strftime("%M:%S:%f")[:-3],
+                    "finishes": len(group),
+                }
+            ]
+        )
+        res = pd.concat([res, item])
+    results = res
+
+    gaps["bins"] = pd.cut(gaps["gap"], bins)
+    res = pd.DataFrame()
+    grouped = gaps.groupby(["bins"])
+    for name, group in grouped:
+        item = pd.DataFrame(
+            [
+                {
+                    "bin": name,
+                    "binLabel": pd.to_datetime(
+                        name.right.astype(np.int64), unit="ms"
+                    ).strftime("%M:%S:%f")[:-3],
                     "accidents": group["accidents"].sum(),
                     "collisions": group["collisions"].sum(),
                 }
@@ -235,36 +361,49 @@ def _analyze_gaps(df: pd.DataFrame) -> None:
         )
         res = pd.concat([res, item])
 
-    # Display text
+    results = (
+        results.merge(res, on=["binLabel"], how="left")
+        .drop(columns=["bin_y"])
+        .rename(columns={"bin_x": "bin"})
+    )
+    del res
+
+    corr_fa = round(results["finishes"].corr(results["accidents"]) * 100, 2)
+    corr_fc = round(results["finishes"].corr(results["collisions"]) * 100, 2)
+    corr_ac = round(results["accidents"].corr(results["collisions"]) * 100, 2)
     text = textwrap.dedent(
         f"""
-        {res['accidents'].max()} Accidents and {res['collisions'].max()} Collisions.
-        {round(res["accidents"].corr(res["collisions"]) * 100, 2)}% correlation.
+        {ta} Accidents and {tc} Collisions.
+        Bins determined by average median gap at the end of the race.
+        Each bin represents 50 finished races.
+        Correlation: (FA: {corr_fa}%, FC: {corr_fc}%, AC: {corr_ac}%).
         """
     )
 
-    width = 0.35
-    x = np.arange(len(res["binLabel"]))
+    x = np.arange(len(results["binLabel"]))
     _, ax = plt.subplots(figsize=IMAGES_DNFS_SIZE, dpi=IMAGES_DPI)
-    ax.bar(x - width / 2, res["accidents"], width, label="Accidents", color="g")
-    ax.bar(x + width / 2, res["collisions"], width, label="Collisions", color="r")
-    ax.set_xticks(x + width * 1.5)
-    ax.set_xticklabels(res["binLabel"])
+    ax.bar(
+        x - BAR_WIDTH / 2, results["accidents"], BAR_WIDTH, label="Accidents", color="g"
+    )
+    ax.bar(x, results["finishes"], BAR_WIDTH, label="Finishes", color="b")
+    ax.bar(
+        x + BAR_WIDTH / 2,
+        results["collisions"],
+        BAR_WIDTH,
+        label="Collisions",
+        color="r",
+    )
+    ax.set_xticks(x + BAR_WIDTH * 1.5)
+    ax.set_xticklabels(results["binLabel"])
     ax.legend()
-    plt.title("Accidents and Collisions over median gap to leader")
+    plt.title("Accidents and Collisions over median gap to leader (equal finishes bin)")
     plt.xlabel("Median gap to leader (upper/right interval limit)")
-    plt.ylabel("DNFS")
     plt.text(
-        0.5,
-        0.99,
-        text,
-        ha="center",
-        va="top",
-        transform=ax.transAxes,
+        0.5, 0.99, text, ha="center", va="top", transform=ax.transAxes, fontsize=12
     )
     print(text)
     plt.savefig(
-        f"{IMAGES_DNFS_FOLDER}/gaps.png",
+        f"{IMAGES_DNFS_FOLDER}/results.png",
         dpi=IMAGES_DPI,
     )
 
@@ -276,16 +415,19 @@ def analyze(force_generate_dataset: bool = False) -> None:
     if (
         not os.path.exists(PERCENTAGES_CSV)
         or not os.path.exists(GAPS_CSV)
+        or not os.path.exists(RESULTS_CSV)
         or force_generate_dataset
     ):
         print("DNFs CSVs not found, generating dataset.")
-        percentages, gaps = generate_dataset()
+        percentages, gaps, results = generate_dataset()
     else:
         percentages = pd.read_csv(PERCENTAGES_CSV)
         gaps = pd.read_csv(GAPS_CSV)
+        results = pd.read_csv(RESULTS_CSV)
 
     _analyze_percentages(percentages)
-    _analyze_gaps(gaps)
+    _analyze_gaps(gaps, results)
+    _analyze_results(results, gaps)
 
 
 if __name__ == "__main__":
